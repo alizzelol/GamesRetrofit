e53d9bb4b669d7df4d6405eb3b7ab9565d259ebd
alizzelol <alizzelol@gmail.com>
2025-04-04 19:25:55 +0200

    name state

--- app/src/main/java/com/alizzelol/gamesretrofit/viewModel/GamesViewModel.kt
+++ app/src/main/java/com/alizzelol/gamesretrofit/viewModel/GamesViewModel.kt
@@ -1,9 +1,13 @@
 package com.alizzelol.gamesretrofit.viewModel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.alizzelol.gamesretrofit.model.GameList
 import com.alizzelol.gamesretrofit.repository.GamesRepository
+import com.alizzelol.gamesretrofit.state.GameState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -18,6 +22,9 @@ class GamesViewModel @Inject constructor(private val repo: GamesRepository) : Vi
     private val _games = MutableStateFlow<List<GameList>>(emptyList())
     val games = _games.asStateFlow()
 
+    var state by mutableStateOf(GameState())
+        private set
+
     init {
         fetchGames() //Conseguir juegos
     }
@@ -27,6 +34,22 @@ class GamesViewModel @Inject constructor(private val repo: GamesRepository) : Vi
             withContext(Dispatchers.IO){
                 val result = repo.getGames()
                 _games.value = result ?: emptyList() //Si no hay resultado, lista vacía
+            }
+        }
+    }
+
+    fun getGameById(id:Int){
+        viewModelScope.launch {
+            withContext(Dispatchers.IO){
+                val result = repo.getGameById(id)
+                state = state.copy(
+                    name = result?.name ?: "",
+                    description_raw = result?.description_raw ?: "",
+                    metacritic = result?.metacritic ?: 111, //111 para superar el 100, max puntuación
+                    website = result?.website ?: "",
+                    background_image = result?.background_image ?: "",
+
+                )
             }
         }
     }
