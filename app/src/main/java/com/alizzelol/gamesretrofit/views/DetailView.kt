3f66a7953b552ec431b0ea9a1bd3a0e9a41841e2
alizzelol <alizzelol@gmail.com>
2025-04-04 19:44:08 +0200

    name + image = DetailView

--- app/src/main/java/com/alizzelol/gamesretrofit/views/DetailView.kt
+++ app/src/main/java/com/alizzelol/gamesretrofit/views/DetailView.kt
@@ -1,21 +1,60 @@
 package com.alizzelol.gamesretrofit.views
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.alizzelol.gamesretrofit.components.MainImage
+import com.alizzelol.gamesretrofit.components.MainTopBar
 import com.alizzelol.gamesretrofit.viewModel.GamesViewModel
+import com.alizzelol.gamesretrofit.util.Constants.Companion.CUSTOM_BLACK
 
 @Composable
 fun DetailView(viewModel: GamesViewModel, navController: NavController, id : Int){
     LaunchedEffect(Unit) {
         viewModel.getGameById(id)
     }
-    Text(text = viewModel.state.name, color = Color.Black)
+    Scaffold(
+        topBar = {
+            MainTopBar(title = viewModel.state.name, showBackButton = true) {
+                navController.popBackStack() //para que regrese hacia atrás
+            }
+        }
+
+    ) {
+        ContentDetailView(it, viewModel)
+    }
 }
 
 @Composable
-fun ContentDetailView(){
+fun ContentDetailView(pad:PaddingValues, viewModel: GamesViewModel){
+    val state = viewModel.state
+
+    Column(modifier = Modifier.padding(pad)
+        .background(Color(CUSTOM_BLACK))
+    ) {
+        MainImage(image = state.background_image)
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp, end = 5.dp)
+        ) {
+
+        }
+    }
 
 }
