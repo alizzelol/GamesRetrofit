12a087d444807aeb667da75f91eac63bf17c3a58
alizzelol <alizzelol@gmail.com>
2025-04-08 19:54:51 +0200

    Crear buscador

--- app/src/main/java/com/alizzelol/gamesretrofit/views/HomeView.kt
+++ app/src/main/java/com/alizzelol/gamesretrofit/views/HomeView.kt
@@ -1,18 +1,30 @@
 package com.alizzelol.gamesretrofit.views
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.alizzelol.gamesretrofit.components.CardGame
@@ -38,20 +50,41 @@ fun HomeView(viewModel: GamesViewModel, navController: NavController){
 @Composable
 fun ContentHomeView(viewModel: GamesViewModel, pad:PaddingValues, navController: NavController){
     val games by viewModel.games.collectAsState()
-    LazyColumn (
-        modifier = Modifier
-            .padding(pad)
-            .background(Color(CUSTOM_BLACK))
-    ) {
-        items(games){ item ->
-            CardGame(item) {
-                navController.navigate("DetailView/${item.id}") //llevar id del item
+    var search by remember { mutableStateOf("") }
+
+    Column(modifier = Modifier.padding(pad),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        TextField(value = search,  //TextField Search para buscar
+            onValueChange = {search = it},
+            label = { Text(text = "Search")},
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+
+                }
+            ),
+            modifier = Modifier.fillMaxWidth()
+                .padding(start = 10.dp, end = 10.dp),
+        )
+
+        LazyColumn (
+            modifier = Modifier
+                .background(Color(CUSTOM_BLACK))
+        ) {
+            items(games){ item ->
+                CardGame(item) {
+                    navController.navigate("DetailView/${item.id}") //llevar id del item
+                }
+                Text(text = item.name,
+                    fontWeight = FontWeight.ExtraBold,
+                    color = Color.White,
+                    modifier = Modifier.padding(start = 10.dp)
+                )
             }
-            Text(text = item.name,
-                fontWeight = FontWeight.ExtraBold,
-                color = Color.White,
-                modifier = Modifier.padding(start = 10.dp)
-            )
         }
     }
 }
